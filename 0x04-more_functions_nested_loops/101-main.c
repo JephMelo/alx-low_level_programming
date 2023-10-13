448174fe72acb487120c95e438bc3be8843fa004
JephMelo <jephthax@gmail.com>
2023-10-13 02:10:23 -0700

    101-main.c

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -6,11 +6,10 @@
  * Return: Always 0.
  */
 int main(void)
-
 {
 	print_number(98);
 	putchar('\n');
-	print_number(402);
+	prin_number(402);
 	putchar('\n');
 	print_number(1024);
 	putchar('\n');
@@ -20,3 +19,4 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
