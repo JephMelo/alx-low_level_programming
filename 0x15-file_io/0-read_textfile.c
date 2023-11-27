ab863fca181abb54af9d12a64b0df59a23156427
JephMelo <jephthax@gmail.com>
2023-11-27 07:29:14 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fontl.h>
+#include <fcntl.h>
 #include <stdlib.h>
 
 /**
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <fontl.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 int _putchar(char c);
