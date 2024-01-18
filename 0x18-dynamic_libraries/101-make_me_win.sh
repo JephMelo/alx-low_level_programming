899b8d1e797744d0e7c37773d9b8d9cce27b0328
JephMelo <jephthax@gmail.com>
2024-01-18 07:46:16 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P 
+wget -P .. https://raw.githubusercontent.com/JephMelo/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD="$PWD/../libhack.so"
